26aea6c80f20c9da15b7eb2b9eaa5b2c8a18eddc
LeoMoschner <l.espindolamoschner@gmail.com>
2022-08-29 16:18:34 -0300

    Dynamic Load Tests

--- src/test/java/com/solvd/webAutomation/BaseTests.java
+++ src/test/java/com/solvd/webAutomation/BaseTests.java
@@ -2,7 +2,6 @@ package com.solvd.webAutomation;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import com.solvd.webAutomation.service.SessionPool;
 import org.openqa.selenium.WebDriver;
@@ -11,16 +10,12 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTests {
-    private WebDriver driver;
-    @BeforeClass
-    public void openDriver(){
-        driver = SessionPool.getINSTANCE().getDriver();
-    }
+    private WebDriver driver = SessionPool.getINSTANCE().getDriver();
     @BeforeMethod
     public void setUp() {
         driver.get("https://the-internet.herokuapp.com/");
     }
-    @AfterClass
+    @AfterSuite
     public void closeSession(){
         driver.close();
     }
