cf07e637509f9bb6423c96eb5c46674736f03395
LeoMoschner <l.espindolamoschner@gmail.com>
2022-08-30 15:57:03 -0300

    Frame Tests. Screenshots on test failures.

--- src/main/java/com/solvd/webAutomation/pages/LoginPage.java
+++ src/main/java/com/solvd/webAutomation/pages/LoginPage.java
@@ -1,13 +1,12 @@
 package com.solvd.webAutomation.pages;
 
-import com.solvd.webAutomation.utils.HomePageLinks;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage extends BasicPage{
-    private By userName = By.id("username");
-    private By password = By.id("password");
-    private By loginButton = By.tagName("button");
+    private final By userName = By.id("username");
+    private final By password = By.id("password");
+    private final By loginButton = By.tagName("button");
     public LoginPage(WebDriver driver) {
         super(driver);
     }
